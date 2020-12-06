b080ecb6d3802ab34da1ed7cb2728410c06da4ec
AtlasC0R3 <39468657+atlasc0r3@users.noreply.github.com>
2020-12-06 16:06:07 -0500

    Updated activity changer script

--- activity.py
+++ activity.py
@@ -1,24 +1,24 @@
 import discord
 import json
-from discord.ext import commands
-
-bot = commands.Bot(
-    command_prefix='+',
-    description='Unavailable: Please don\'t try again later.',
-    owner_id=333027024576970755,
-    case_insensitive=True
-)
 
 
-@bot.event
-async def on_ready():
-    with open("data/activities.json", encoding='utf-8', newline="\n") as f:
-        dictionary = json.load(f)
-    activitynumber = input('give activity id:\n')
-    activity = list(dictionary.values())[int(activitynumber)]
-    activitytype = activity[0]
-    activityname = activity[1]
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType[activitytype], name=activityname))
-    quit()
+class MyClient(discord.Client):
+    async def on_ready(self):
+        with open("data/activities.json", encoding='utf-8', newline="\n") as f:
+            activities = json.load(f)
+        activitynumber = input('Please input your activity index:\n')
+        try:
+            activity = list(activities)[int(activitynumber)]
+        except IndexError:
+            exit("That wasn't a valid index. Rather than running again I'm aborting now.\n"
+                 "Make sure you inputted your values correctly.")
+            return
+        activitytype = activity[0]
+        activityname = activity[1]
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType[activitytype],
+                                                               name=activityname))
+        exit(1)
 
-bot.run(open("data/token.txt", "rt").read(), bot=True, reconnect=True)
+
+client = MyClient()
+client.run(open("data/token.txt", "rt").read(), bot=True, reconnect=False)
